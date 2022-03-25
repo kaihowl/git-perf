ce6ebf1406e6cb5c9499d9aaf8f8f35680281141
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2022-03-25 07:02:12 +0100

    fix: directory independent invocation

--- report.sh
+++ report.sh
@@ -3,6 +3,7 @@
 set -e
 set -x
 
+script_dir=$(dirname "$0")
 
 git fetch origin "refs/notes/perf:refs/notes/perf"
-git --no-pager log --no-color --pretty='-- %H%n%N' --notes=refs/notes/perf | python3 report.py
+git --no-pager log --no-color --pretty='-- %H%n%N' --notes=refs/notes/perf | python3 "${script_dir}/report.py"
