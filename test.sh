63b267e8747d644c5c39c45b6b84b4cbc6aa90a1
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2022-04-16 19:58:18 +0200

    feat: allow pruning of perf notes of unreachable/non-existent objects

--- test.sh
+++ test.sh
@@ -443,5 +443,50 @@ git perf add -m timer 2
 git perf audit -m timer && exit 1
 
 
+## Check git perf prune functionality
+
+# Refuse to run on a shallow clone
+cd_temp_repo
+repo=$(pwd)
+
+create_commit
+create_commit
+create_commit
+cd "$(mktemp -d)"
+git init
+git remote add origin "${repo}"
+git fetch --no-tags --prune --progress --no-recurse-submodules --depth=1 --update-head-ok origin master:master
+output=$(git perf prune 2>&1 1>/dev/null) && exit 1
+if [[ ${output} != *'shallow'* ]]; then
+  echo No warning for 'shallow' clone
+  echo "$output"
+  exit 1
+fi
+
+# Normal operations on main repo
+cd_temp_repo
+create_commit
+git perf add -m test 5
+git perf prune
+nr_notes=$(git notes --ref=perf list | wc -l)
+if [[ $nr_notes -ne 1 ]]; then
+  echo Expected to have 1 note but found "$nr_notes" instead
+  exit 1
+fi
+git reset --hard HEAD~1
+nr_notes=$(git notes --ref=perf list | wc -l)
+if [[ $nr_notes -ne 1 ]]; then
+  echo Expected to have 1 note but found "$nr_notes" instead
+  exit 1
+fi
+git reflog expire --expire-unreachable=now --all
+git prune --expire=now
+git perf prune
+nr_notes=$(git notes --ref=perf list | wc -l)
+if [[ $nr_notes -ne 0 ]]; then
+  echo Expected to have no notes but found "$nr_notes" instead
+  exit 1
+fi
+
 exit 0
 
