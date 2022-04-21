b1e7edda323ba81a925d87848ed6fff4e649e9e3
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2022-04-21 10:03:09 +0200

    use gnu parallel

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -5,28 +5,5 @@ script_dir=$(dirname "$0")
 
 python3 -m pip install -r "${script_dir}/../requirements.txt"
 
-tests=()
-
-# TODO(kaihowl) no limit
-for file in "${script_dir}"/test_*.sh; do
-  echo "Running test $file..."
-  log_file=$(mktemp)
-  "./$file" > "${log_file}" 2>&1 &
-  tests+=("$! $log_file $file")
-done
-
-exit=0
-for test in "${tests[@]}"; do
-  IFS=' ' read -r pid logfile file <<< "${test}"
-  echo "Waiting for $file..."
-  if ! wait "$pid"; then
-    echo "Failed."
-    cat "$logfile" | while read line; do echo "[$file] $line"; done
-    exit=1
-  else
-    echo "Success."
-  fi
-done
-
-exit $exit
-
+num_procs=4
+find "${script_dir}" -name 'test_*.sh' -print0 | parallel -n1 -P${num_procs} --tag -0 bash {}
