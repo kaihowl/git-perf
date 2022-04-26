565ee8a7c390f99fd9919e0ea6d3c99f84d3473b
Kai Howelmeyer <kai@hoewelmeyer.eu>
2022-04-26 07:31:06 +0200

    test: add test for shallow clones of PRs (#27)

--- test/test_shallow.sh
+++ test/test_shallow.sh
@@ -34,3 +34,38 @@ if [[ ${output} != *'shallow clone'* ]]; then
   echo "$output"
   exit 1
 fi
+
+# The shallow warning for a PR-branch with a merge as HEAD should be counting the first parents history.
+# This is already the default behavior for git-fetch with the depth option.
+cd_temp_repo
+full_repo=$(pwd)
+for i in $(seq 1 10); do
+  create_commit
+  git perf add -m test-measure 5
+done
+git checkout -b feature_branch
+for i in $(seq 1 5); do
+  create_commit
+done
+git checkout -
+git merge --no-ff -
+# Test first-parent fetch-depth behavior even if HEAD is non-merge commit.
+create_commit
+# Shallow clone with depth == 10 for main branch
+cd "$(mktemp -d)"
+git clone "file://$full_repo" --depth=10 shallow_clone
+cd shallow_clone
+# Must fail as this expects more history
+output=$(git perf report -n 11 2>&1 1>/dev/null) && exit 1
+if [[ ${output} != *'shallow clone'* ]]; then
+  echo Missing warning for 'shallow clone'
+  echo "$output"
+  exit 1
+fi
+git perf pull
+# Must work as this is the exact history length
+# If we erroneously considered the feature_branch's history, it would be filtered
+# out and we end up with fewer than 10 commits when following the first parent.
+git perf report -n 10
+
+exit 0
