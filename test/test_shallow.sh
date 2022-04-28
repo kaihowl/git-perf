cc60f61b703f064cb76493388ff0d1cf1fd36161
Kai Howelmeyer <kai@hoewelmeyer.eu>
2022-04-28 08:08:11 +0200

    fix: align meaning of '-n' flag for audit and report (#28)
    Both should consider the same commits. Report just takes a look back
    for <n> commits including HEAD. Audit looks at the same range, but
    separates HEAD and the tail commits for correct statistics.

--- test/test_shallow.sh
+++ test/test_shallow.sh
@@ -21,21 +21,22 @@ cd "$(mktemp -d)"
 git clone "file://$full_repo" --depth=2 shallow_clone
 cd shallow_clone
 git perf pull
-git perf report -n 1
-output=$(git perf report -n 10 2>&1 1>/dev/null) && exit 1
+git perf report -n 2
+git perf audit -n 2 -m test-measure
+output=$(git perf report -n 3 2>&1 1>/dev/null) && exit 1
 if [[ ${output} != *'shallow clone'* ]]; then
   echo Missing warning for 'shallow clone'
   echo "$output"
   exit 1
 fi
-output=$(git perf audit -n 10 -m test-measure 2>&1 1>/dev/null) && exit 1
+output=$(git perf audit -n 3 -m test-measure 2>&1 1>/dev/null) && exit 1
 if [[ ${output} != *'shallow clone'* ]]; then
   echo Missing warning for 'shallow clone'
   echo "$output"
   exit 1
 fi
 
-# The shallow warning for a PR-branch with a merge as HEAD should be counting the first parents history.
+# The shallow warning for a PR-branch with a merge as HEAD should be counting the first parent's history.
 # This is already the default behavior for git-fetch with the depth option.
 cd_temp_repo
 full_repo=$(pwd)
@@ -50,11 +51,15 @@ done
 git checkout -
 git merge --no-ff -
 # Test first-parent fetch-depth behavior even if HEAD is non-merge commit.
-create_commit
+commit=$(git rev-parse HEAD)
 # Shallow clone with depth == 10 for main branch
 cd "$(mktemp -d)"
-git clone "file://$full_repo" --depth=10 shallow_clone
-cd shallow_clone
+git init
+git remote add origin "file://$full_repo"
+# Simulate the behavior of github actions checkout closely
+git fetch origin "$commit:local-ref" --depth=10
+git checkout local-ref
+git perf pull
 # Must fail as this expects more history
 output=$(git perf report -n 11 2>&1 1>/dev/null) && exit 1
 if [[ ${output} != *'shallow clone'* ]]; then
@@ -62,7 +67,6 @@ if [[ ${output} != *'shallow clone'* ]]; then
   echo "$output"
   exit 1
 fi
-git perf pull
 # Must work as this is the exact history length
 # If we erroneously considered the feature_branch's history, it would be filtered
 # out and we end up with fewer than 10 commits when following the first parent.
