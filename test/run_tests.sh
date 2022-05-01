2d40950f91ad1ac52f0e4173d00d8f7739995ccc
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2022-05-01 12:29:02 +0200

    feat: convert to setuptools

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
+set -x
 
 script_dir=$(dirname "$0")
-
-python3 -m pip install -r "${script_dir}/../requirements.txt"
 
 num_procs=4
 find "${script_dir}" -name 'test_*.sh' -print0 | parallel -n1 -P${num_procs} --tag -0 bash {}
