5e430dfeb89bcd70f79de43b5cdfe25280562979
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2022-05-01 12:19:29 +0200

    verbose install

--- test/install.sh
+++ test/install.sh
@@ -4,7 +4,10 @@ set -e
 set -x
 
 if [[ "$(uname -s)" = "Darwin" ]]; then
-  brew install parallel || true
+  # This fails when run as part of tox pre_command on GitHub Actions.
+  # It only fails after the installation of `parallel` is done.
+  # Therefore, ignoring the failure.
+  brew install -v parallel || true
 elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
   sudo apt-get install parallel
 fi
