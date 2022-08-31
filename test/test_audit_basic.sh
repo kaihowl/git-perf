d00b2614dda32bb1025e9c52a373c95008a04d76
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2022-08-31 20:02:55 +0200

    feat: pass audit if there are too few measurements
    In case that only a few measurements have been collected, the stddev
    is not yet stable enough. Therefore, the audit should pass in those
    cases.
    
    Since the number of commits passed by `-n` only determines the
    look-behind of commits scanned, and those commits are not guaranteed to
    all have measurements, a second parameter --min-measurements is added.

--- test/test_audit_basic.sh
+++ test/test_audit_basic.sh
@@ -10,20 +10,43 @@ source "$script_dir/common.sh"
 echo Basic audit tests
 
 cd_temp_repo
+# mean: 2, std: 1
 git checkout HEAD~3
 git perf add -m timer 1
 git checkout - && git checkout HEAD~2
 git perf add -m timer 2
 git checkout - && git checkout HEAD~1
 git perf add -m timer 3
+# head commit
 git checkout -
 git perf add -m timer 4
-# mean: 2, std: 1
+# measure
 git perf audit -m timer -d 4
 git perf audit -m timer -d 3
 git perf audit -m timer -d 2
 git perf audit -m timer -d 1.9999 && exit 1
 git perf audit -m timer -d 1 && exit 1
+
+
+echo Initial measurements with too few data points
+cd_empty_repo
+# mean: 15, std: 5
+create_commit
+git perf add -m timer 10
+create_commit
+git perf add -m timer 15
+create_commit
+git perf add -m timer 20
+# head commit
+create_commit
+git perf add -m timer 30
+# measure
+git perf audit -m timer -d 3
+git perf audit -m timer -d 2 && exit 1
+git perf audit -m timer -d 2 --min-measurements 10
+git perf audit -m timer -d 2 --min-measurements 4
+git perf audit -m timer -d 2 --min-measurements 3 && exit 1
+
 
 echo Stable measurements with zero stddev
 cd_empty_repo
