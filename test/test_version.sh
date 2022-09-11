9abe0f2f7acd5e37f58da00a10bcdb2866c342d2
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2022-09-11 21:35:17 +0200

    feat: add version output

---
+++ test/test_version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+set -x
+
+script_dir=$(dirname "$0")
+# shellcheck source=test/common.sh
+source "$script_dir/common.sh"
+
+output=$(git perf 2>&1 1>/dev/null) && exit 1
+if [[ ${output} != *'are required'* ]]; then
+  echo No warning for missing arguments
+  echo "$output"
+  exit 1
+fi
+
+output=$(git perf --version)
+if ! [[ ${output} =~ \d+\.\d+.\d+ ]]; then
+    echo Expected version number or placeholder in output.
+    echo "$output"
+    exit 1
+fi
