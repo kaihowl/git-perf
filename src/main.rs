9ba706bc0b53a181540654c7cf9af31840cfb923
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2022-10-08 17:34:50 +0200

    feat: add example command line parser from clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Parser)]
+struct Cli {
+    /// Optional name to operate on
+    name: Option<String>,
+
+    /// Set a custom config file
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>,
+
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Does testing things
+    Test {
+        /// list test values
+        #[arg(short, long)]
+        list: bool,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    if let Some(name) = cli.name.as_deref() {
+        println!("Value for name: {}", name);
+    }
+
+    if let Some(config_path) = cli.config.as_deref() {
+        println!("Value for config: {}", config_path.display());
+    }
+
+    match cli.debug {
+        0 => println!("Debug mode is off"),
+        1 => println!("Debug mode is kind of on"),
+        2 => println!("Debug mode is on"),
+        _ => println!("Don't go crazy"),
+    }
+
+    match &cli.command {
+        Some(Commands::Test { list }) => {
+            if *list {
+                println!("Printing lists");
+            } else {
+                println!("Not printing lists");
+            }
+        }
+        None => {}
+    }
 }
