ef80d00da734c703d9ba3626b1812f510e7b46ae
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2022-11-29 11:01:58 +0100

    full on test

--- test/test_pushpull_conflict.sh
+++ test/test_pushpull_conflict.sh
@@ -7,52 +7,93 @@ script_dir=$(dirname "$0")
 # shellcheck source=test/common.sh
 source "$script_dir/common.sh"
 
-echo Setup a bare remote with a single commit
-cd "$(mktemp -d)"
-root=$(pwd)
+# Test case 1: Conflicting on single commit.
+# Test case 2: No conflicts but merge needed.
 
-mkdir orig
-cd orig
-orig=$(pwd)
+# When run, creates and changes into a temp folder with the following structure:
+# - original/  # bare, upstream repo
+# - repo1/     # First working copy
+# - repo2/     # Second working copy
+function bare_repo_and_two_clones() {
+  cd "$(mktemp -d)"
+  mkdir original
+  cd original
+  git init --bare
+  cd ..
 
-git init --bare
+  git clone original repo1
+  git clone original repo2
+}
 
-cd "$(mktemp -d)"
-git clone "$orig" myworkrepo
+echo Test case 1: Conflicting on single commit.
 
-cd myworkrepo
-
-touch a
-git add a
-git commit -m 'first commit'
-
+echo Setup both working copies to have a single shared, upstream commit
+bare_repo_and_two_clones
+pushd repo1
+create_commit
 git push
+popd
+pushd repo2
+git pull
+popd
 
-echo Checkout two working copies
-cd "$root"
-git clone "$orig" repo1
-git clone "$orig" repo2
-repo1=$(pwd)/repo1
-repo2=$(pwd)/repo2
 
-echo In first working copy, add a measurement the commit and push it
-cd "$repo1"
+echo In first working copy, add a perf measurement and push it
+pushd repo1
 git perf add -m echo 0.5 -k repo=first
 git perf push
+popd
 
-echo In the second working copy, add a measurement as well
-cd "$repo2"
+echo In the second working copy, also add a perf measurement to cause a conflict
+pushd repo2
 git perf add -m echo 0.5 -k repo=second
+# There is a conflict, we must pull first
+git perf push && exit 1
+out=$(mktemp).csv && git perf report -o "$out" && cat "$out"
+git perf pull
+out=$(mktemp).csv && git perf report -o "$out" && cat "$out"
+git perf push
+popd
+
+echo In the first working copy, we should also see both measurements now
+pushd repo1
+git perf pull
+out=$(mktemp).csv && git perf report -o "$out" && cat "$out"
+popd
+
+echo Test case 2: No conflicts but merge needed.
+
+echo Setup empty upstream
+bare_repo_and_two_clones
+
+echo In first working copy, add a commit with a perf measurement, publish commit and perf
+pushd repo1
+create_commit
+git perf add -m echo 0.5 -k repo=first
+git push
+git perf push
+popd
+
+echo "In the second working copy, pull (without perf), add commit with perf, publish commit and perf"
+pushd repo2
+git pull
+create_commit
+git perf add -m echo 0.5 -k repo=second
+out=$(mktemp).csv && git perf report -o "$out" && cat "$out"
+git push
+# There is a conflict, we must pull first
 git perf push && exit 1
 git perf pull
+out=$(mktemp).csv && git perf report -o "$out" && cat "$out"
 git perf push
+popd
 
-# TODO(kaihowl) debug
-cd "$repo1"
-out=$(mktemp).csv
+echo In the first working copy, we should also see both measurements on separate commits now
+pushd repo1
+git pull
 git perf pull
-git perf report -o "$out"
-cat "$out"
+out=$(mktemp).csv && git perf report -o "$out" && cat "$out"
+popd
 
 
 exit 0
