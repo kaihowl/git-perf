693e652d4a4b8f21a9a0a75158988f1a0fca70dd
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2022-12-01 10:53:09 +0100

    extend measure command tests

--- test/test_measure.sh
+++ test/test_measure.sh
@@ -17,3 +17,12 @@ if [[ ! ${output} =~ $re ]]; then
   exit 1
 fi
 
+echo Non-existing command
+cd_temp_repo
+git perf measure -m test-measure -- does-not-exist && exit 1
+
+echo Valid command, repeated measurements
+cd_temp_repo
+git perf measure -m test-emasure -n 5 -- true
+num_measurements=$(git perf report -o - | wc -l)
+[[ ${num_measurements} -eq 5 ]] || exit 1
