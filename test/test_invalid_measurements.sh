51be77da430807f601100ad86dacf558b8771610
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2022-12-01 10:52:54 +0100

    split up invalid measurement and measure command tests

---
+++ test/test_invalid_measurements.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+set -e
+set -x
+
+script_dir=$(dirname "$0")
+# shellcheck source=test/common.sh
+source "$script_dir/common.sh"
+
+echo Add invalid measurements
+
+echo Empty measurement
+cd_temp_repo
+git perf add -m echo 0.5
+"${script_dir}/measure.sh" "\n"
+output=$(git perf report 2>&1 1>/dev/null)
+if [[ ${output} != *'too few items'* ]]; then
+  echo Missing 'too few items' in output:
+  echo "$output"
+  exit 1
+fi
+
+echo Measurement with just date
+cd_temp_repo
+git perf add -m echo 0.5
+"${script_dir}/measure.sh" "$(date +%s)"
+output=$(git perf report 2>&1 1>/dev/null)
+if [[ ${output} != *'too few items'* ]]; then
+  echo Missing 'too few items' in output:
+  echo "$output"
+  exit 1
+fi
+
+echo Measurement without date
+cd_temp_repo
+git perf add -m echo 0.5
+"${script_dir}/measure.sh" "myothermeasurement $RANDOM key=value"
+output=$(git perf report 2>&1 1>/dev/null)
+if [[ ${output} != *'found non-numeric value'* ]]; then
+  echo Missing 'found non-numeric value' in output:
+  echo "$output"
+  exit 1
+fi
+
+echo Measurement without kvs
+cd_temp_repo
+git perf add -m echo 0.5
+"${script_dir}/measure.sh" "myothermeasurement $(date +%s) $RANDOM"
+output=$(git perf report 2>&1 1>/dev/null)
+if [[ -n ${output} ]]; then
+  echo There should be no output in stderr but instead there is:
+  echo "$output"
+  exit 1
+fi
+
+echo Measurement with invalid kvs
+cd_temp_repo
+git perf add -m echo 0.5
+"${script_dir}/measure.sh" "myothermeasurement $(date +%s) $RANDOM test othertest stuff"
+output=$(git perf report 2>&1 1>/dev/null)
+if [[ -n ${output} ]]; then
+  echo There should be no output in stderr but instead there is:
+  echo "$output"
+  exit 1
+fi
+
+echo Measurement valid but with too many spaces
+cd_temp_repo
+git perf add -m echo 0.5
+"${script_dir}/measure.sh" "myothermeasurement    $(date +%s)      $RANDOM key=value"
+output=$(git perf report 2>&1 1>/dev/null)
+if [[ -n ${output} ]]; then
+  echo There should be no output in stderr but instead there is:
+  echo "$output"
+  exit 1
+fi
+
+echo Duplicate kvs
+cd_temp_repo
+git perf add -m echo 0.5
+"${script_dir}/measure.sh" "myothermeasurement $(date +%s) $RANDOM key=value key=value"
+output=$(git perf report 2>&1 1>/dev/null)
+if [[ -n ${output} ]]; then
+  echo There should be no output in stderr but instead there is:
+  echo "$output"
+  exit 1
+fi
+
+echo Conflicting kvs
+cd_temp_repo
+git perf add -m echo 0.5
+"${script_dir}/measure.sh" "myothermeasurement $(date +%s) $RANDOM key=value key=value2"
+output=$(git perf report 2>&1 1>/dev/null)
+if [[ -n ${output} ]]; then
+  echo There should be no output in stderr but instead there is:
+  echo "$output"
+  exit 1
+fi
--- test/test_measure.sh
+++ test/test_measure.sh
@@ -7,6 +7,7 @@ script_dir=$(dirname "$0")
 # shellcheck source=test/common.sh
 source "$script_dir/common.sh"
 
+echo Missing arguments
 cd_temp_repo
 output=$(git perf measure -m test-measure 2>&1 1>/dev/null) && exit 1
 re=".*following (required )?arguments.*"
@@ -16,92 +17,3 @@ if [[ ! ${output} =~ $re ]]; then
   exit 1
 fi
 
-echo Add invalid measurements
-
-echo Empty measurement
-cd_temp_repo
-git perf add -m echo 0.5
-"${script_dir}/measure.sh" "\n"
-output=$(git perf report 2>&1 1>/dev/null)
-if [[ ${output} != *'too few items'* ]]; then
-  echo Missing 'too few items' in output:
-  echo "$output"
-  exit 1
-fi
-
-echo Measurement with just date
-cd_temp_repo
-git perf add -m echo 0.5
-"${script_dir}/measure.sh" "$(date +%s)"
-output=$(git perf report 2>&1 1>/dev/null)
-if [[ ${output} != *'too few items'* ]]; then
-  echo Missing 'too few items' in output:
-  echo "$output"
-  exit 1
-fi
-
-echo Measurement without date
-cd_temp_repo
-git perf add -m echo 0.5
-"${script_dir}/measure.sh" "myothermeasurement $RANDOM key=value"
-output=$(git perf report 2>&1 1>/dev/null)
-if [[ ${output} != *'found non-numeric value'* ]]; then
-  echo Missing 'found non-numeric value' in output:
-  echo "$output"
-  exit 1
-fi
-
-echo Measurement without kvs
-cd_temp_repo
-git perf add -m echo 0.5
-"${script_dir}/measure.sh" "myothermeasurement $(date +%s) $RANDOM"
-output=$(git perf report 2>&1 1>/dev/null)
-if [[ -n ${output} ]]; then
-  echo There should be no output in stderr but instead there is:
-  echo "$output"
-  exit 1
-fi
-
-echo Measurement with invalid kvs
-cd_temp_repo
-git perf add -m echo 0.5
-"${script_dir}/measure.sh" "myothermeasurement $(date +%s) $RANDOM test othertest stuff"
-output=$(git perf report 2>&1 1>/dev/null)
-if [[ -n ${output} ]]; then
-  echo There should be no output in stderr but instead there is:
-  echo "$output"
-  exit 1
-fi
-
-echo Measurement valid but with too many spaces
-cd_temp_repo
-git perf add -m echo 0.5
-"${script_dir}/measure.sh" "myothermeasurement    $(date +%s)      $RANDOM key=value"
-output=$(git perf report 2>&1 1>/dev/null)
-if [[ -n ${output} ]]; then
-  echo There should be no output in stderr but instead there is:
-  echo "$output"
-  exit 1
-fi
-
-echo Duplicate kvs
-cd_temp_repo
-git perf add -m echo 0.5
-"${script_dir}/measure.sh" "myothermeasurement $(date +%s) $RANDOM key=value key=value"
-output=$(git perf report 2>&1 1>/dev/null)
-if [[ -n ${output} ]]; then
-  echo There should be no output in stderr but instead there is:
-  echo "$output"
-  exit 1
-fi
-
-echo Conflicting kvs
-cd_temp_repo
-git perf add -m echo 0.5
-"${script_dir}/measure.sh" "myothermeasurement $(date +%s) $RANDOM key=value key=value2"
-output=$(git perf report 2>&1 1>/dev/null)
-if [[ -n ${output} ]]; then
-  echo There should be no output in stderr but instead there is:
-  echo "$output"
-  exit 1
-fi
