8bf71102f8151dc75799db88746ffe7e3b67cf96
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2023-11-18 12:29:43 +0100

    rewrite run_tests, skip parallel dependency, clean output

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,8 +1,42 @@
 #!/bin/bash
 
-set -x
+set -e
 
 script_dir=$(dirname "$0")
-
 num_procs=4
-find "${script_dir}" -name 'test_*.sh' -print0 | parallel --group -n1 -P${num_procs} --tag -0 bash {}
+
+# Function to execute command and capture output
+execute_command() {
+    local output_file
+    output_file=$(mktemp)
+    # Trap to clean up temporary file on exit
+    trap 'rm -f "$output_file"' EXIT
+
+    echo "Running '$*'"
+
+    if ! "$@" > "$output_file" 2>&1; then
+        echo "Command '$*' failed. Output:"
+        cat "$output_file"
+        exit 1
+    fi
+}
+
+# Find command to generate the list of scripts
+commands=()
+while IFS= read -r -d '' file; do
+    commands+=("$file")
+done < <(find "${script_dir}" -name 'test_*.sh' -print0)
+
+# Execute commands in parallel with a maximum of 3 processes at a time
+for cmd in "${commands[@]}"; do
+    execute_command bash -c "$cmd" &
+    # Limit the number of concurrent processes
+    while [ $(jobs -p | wc -l) -ge $num_procs ]; do
+        sleep 1
+    done
+done
+
+# Wait for all background jobs to finish
+wait
+echo "All commands executed successfully."
+
