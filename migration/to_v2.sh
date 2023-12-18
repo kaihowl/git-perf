b8b368b07e39fe02c3eb7ffaf1794b3e79bc5796
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2023-12-18 18:59:21 +0100

    add migration script

---
+++ migration/to_v2.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -e
+
+# Check if no argument is provided
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <directory>"
+    exit 1
+fi
+
+directory=$1
+
+# Check if the directory exists
+if [ ! -d "$directory" ]; then
+    echo "Directory '$directory' not found."
+    exit 1
+fi
+
+# Check if the directory is a Git repository
+if [ ! -d "$directory/.git" ]; then
+    echo "Directory '$directory' is not a Git repository."
+    exit 1
+fi
+
+echo "Directory '$directory' is a Git repository."
+
+tmpdir=$(mktemp -d)
+
+pushd "$tmpdir"
+
+trap 'rm -r $tmpdir' EXIT
+
+git clone "${directory}" test-repo
+
+cd test-repo
+
+git fetch origin refs/notes/perf:refs/notes/perf
+
+git checkout refs/notes/perf
+
+process_file() {
+  perl -pe 's/^/0 /' -i "$1"
+  git add "$1"
+}
+
+git ls-tree -r --name-only HEAD | while read -r file; do
+    if [ ! -d "$file" ]; then
+        process_file "$file"
+    fi
+done
+
+git commit -m 'migrate to v2'
+
+git push origin HEAD:refs/notes/perf-v2
