9d6195acca23cae4faf8a00d02fd9725c59e8fc2
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2024-01-01 17:24:42 +0100

    add failing test

---
+++ test/test_broken_pipe.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+set -x
+
+script_dir=$(dirname "$0")
+# shellcheck source=test/common.sh
+source "$script_dir/common.sh"
+
+echo Output into broken pipe
+cd_empty_repo
+create_commit
+git perf add -m test 2
+create_commit
+git perf add -m test 4
+create_commit
+git perf report -o - | false
