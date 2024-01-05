9adbbb425bf32909723f65bc41c1dcb1415829f7
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2024-01-05 16:53:26 +0100

    rename and reuse copy as 'read' benchmark

---
+++ benches/read.rs
@@ -0,0 +1,75 @@
+use std::{
+    env::{self, set_current_dir},
+    process::Command,
+};
+
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use git_perf::{
+    data::ReductionFunc,
+    measurement_retrieval::{self, summarize_measurements},
+};
+use tempfile::tempdir;
+
+fn hermetic_git_env() {
+    env::set_var("GIT_CONFIG_NOSYSTEM", "true");
+    env::set_var("GIT_CONFIG_GLOBAL", "/dev/null");
+    env::set_var("GIT_AUTHOR_NAME", "testuser");
+    env::set_var("GIT_AUTHOR_EMAIL", "testuser@example.com");
+    env::set_var("GIT_COMMITTER_NAME", "testuser");
+    env::set_var("GIT_COMMITTER_EMAIL", "testuser@example.com");
+}
+
+fn prep_repo(number_commits: usize, number_measurements: usize) -> tempfile::TempDir {
+    let temp_dir = tempdir().unwrap();
+
+    set_current_dir(temp_dir.path()).expect("Failed to change current path");
+    hermetic_git_env();
+
+    assert!(Command::new("git")
+        .arg("init")
+        .output()
+        .expect("Failed to init git repo")
+        .status
+        .success());
+
+    for _ in 1..number_commits {
+        assert!(Command::new("git")
+            .args(["commit", "--allow-empty", "-m", "test commit"])
+            .output()
+            .expect("Failed to init repo")
+            .status
+            .success());
+
+        let measurements = [10.0].repeat(number_measurements);
+        git_perf::measurement_storage::add_multiple("test_measurement", &measurements, &[])
+            .expect("Could not add measurements");
+    }
+
+    temp_dir
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("read");
+    let num_commits = 40;
+
+    for num_measurements in [10, 100, 500].iter() {
+        let _temp_dir = prep_repo(num_commits, *num_measurements);
+
+        group.throughput(Throughput::Elements(*num_measurements as u64));
+        group.bench_function(BenchmarkId::new("read", num_measurements), |b| {
+            b.iter(|| {
+                let measurements = measurement_retrieval::walk_commits(num_commits)
+                    .expect("Could not get measurements");
+                let summaries =
+                    summarize_measurements(measurements, &ReductionFunc::Min, &|_| true);
+                git_perf::stats::aggregate_measurements(
+                    summaries.map(|x| x.unwrap().measurement.unwrap().val),
+                );
+            })
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
