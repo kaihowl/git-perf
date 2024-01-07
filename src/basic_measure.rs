20bfcf09d9800487d5b41394cbd5728da077718f
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2024-01-07 17:01:17 +0100

    better error output on run errors

--- src/basic_measure.rs
+++ src/basic_measure.rs
@@ -1,6 +1,6 @@
 use std::{process, time::Instant};
 
-use anyhow::Result;
+use anyhow::{bail, Context, Result};
 
 use crate::measurement_storage::{self};
 
@@ -16,15 +16,16 @@ pub fn measure(
         let mut process = process::Command::new(exe);
         process.args(args);
         let start = Instant::now();
-        let output = process
-            .output()
-            .expect("Command failed to spawn TODO(kaihowl)");
-        output
-            .status
-            .success()
-            .then_some(())
-            .ok_or("TODO(kaihowl) running error")
-            .expect("TODO(kaihowl)");
+        let output = process.output().context("Command failed to spawn")?;
+        if !output.status.success() {
+            let stdout = String::from_utf8_lossy(&output.stdout);
+            let stderr = String::from_utf8_lossy(&output.stderr);
+            bail!(
+                "Command failed to run:\nstdout:\n{}\nstderr:\n{}",
+                stdout,
+                stderr,
+            );
+        }
         let duration = start.elapsed();
         let duration_usec = duration.as_nanos() as f64;
         measurement_storage::add(measurement, duration_usec, key_values)?;
