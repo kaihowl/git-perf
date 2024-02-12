b935a40171136b948d061cbf28084a9e00f4f2e0
Kai Howelmeyer <kai@hoewelmeyer.eu>
2024-02-12 21:27:16 +0100

    feat: check git version before operation (#102)
    Fixes #102

--- test/test_version.sh
+++ test/test_version.sh
@@ -20,3 +20,13 @@ if ! [[ ${output} =~ ^(git-perf )?[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     echo "$output"
     exit 1
 fi
+
+# Git version too old
+export PATH=${script_dir}/fake_git_2.40.0:$PATH
+git-perf --version && exit 1
+
+# Git version just right
+export PATH=${script_dir}/fake_git_2.41.0:$PATH
+git-perf --version
+
+exit 0
