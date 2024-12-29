2806120138fcfd3267d501fe14ad561647f223b1
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2024-12-29 16:38:26 +0100

    more specific expectations

--- test/test_remove.sh
+++ test/test_remove.sh
@@ -21,7 +21,7 @@ export FAKETIME='-28d'
 
 echo Add measurement on commit in the past
 create_commit 
-git perf add -m test-measure 10.0
+git perf add -m test-measure-one 10.0
 num_measurements=$(git perf report -o - | wc -l)
 # Exactly one measurement should be present
 [[ ${num_measurements} -eq 1 ]] || exit 1
@@ -39,7 +39,7 @@ export FAKETIME='-14d'
 
 echo "Add a commit with a newer measurement"
 create_commit
-git perf add -m test-measure 20.0
+git perf add -m test-measure-two 20.0
 num_measurements=$(git perf report -o - | wc -l)
 # Two measurements should be there
 [[ ${num_measurements} -eq 2 ]] || exit 1
@@ -64,7 +64,7 @@ num_measurements=$(git perf report -o - | wc -l)
 
 echo "Add a commit with a newer measurement"
 create_commit
-git perf add -m test-measure 30.0
+git perf add -m test-measure-three 30.0
 
 num_measurements=$(git perf report -o - | wc -l)
 # One measurement should be there
@@ -109,7 +109,33 @@ git prune --expire=now
 cur_objects=$(git count-objects -v | awk '/count:/ { print $2 }')
 cur_in_pack=$(git count-objects -v | awk '/in-pack:/ { print $2 }')
 
-# TODO(kaihowl) check that no object contains measurements for the first commit anymore
+# Check that no git objects in the entire repo contains the given pattern
+function git_objects_contain {
+  PATTERN=$1
+  # Iterate over all git objects in the repository
+  while read -r OBJECT; do
+      # Check if the object contains the specific pattern
+      if git cat-file -p "$OBJECT" 2>/dev/null | grep -q "$PATTERN"; then
+          echo "The pattern '$PATTERN' was found in the git object '$OBJECT'."
+          return 0
+      fi
+  done < <(git rev-list --objects --all | awk '{print $1}')
+
+  echo "Success: No git objects contain the pattern '$PATTERN'."
+  return 1
+}
+
+if git_objects_contain test-measure-one; then
+  echo "Unexpectedly still found test-measure-one in the git objects"
+  echo "This should have been already pruned"
+  exit 1
+fi
+
+if ! git_objects_contain test-measure-two; then
+  echo "Expected to find no longer visible but not yet garbage collected test-measure-two in the repo"
+  exit 1
+fi
+
 if ! [[ $((cur_objects + cur_in_pack)) -lt $((prev_objects + prev_in_pack)) ]]; then
   echo "The number of objects now ($cur_objects + $cur_in_pack)
   is not less than previously ($prev_objects + $prev_in_pack)"
