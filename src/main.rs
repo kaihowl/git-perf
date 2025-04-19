dcb32c9bf368883a1e90cb5701ebc40e74c947be
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2025-04-19 13:55:35 +0200

    fix up default logging level

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use anyhow::Result;
+use env_logger::Env;
 use git_perf::cli;
 
 fn main() -> Result<()> {
-    env_logger::init();
+    env_logger::Builder::from_env(Env::default().default_filter_or("warn")).init();
     cli::handle_calls()
 }
