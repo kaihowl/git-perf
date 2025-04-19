922b562579a0e8bfc3189fb48ddaac9f105f5176
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2025-04-19 19:56:19 +0200

    configure logging level

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use anyhow::Result;
-use env_logger::Env;
 use git_perf::cli;
 
 fn main() -> Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("warn")).init();
     cli::handle_calls()
 }
