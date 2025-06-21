a73e256a528fce4ca6a63b1fa414520edc40ffd7
Kai Howelmeyer <kai@hoewelmeyer.eu>
2025-06-21 22:28:34 +0200

    refactor: remove outdated TODO comment in raw_add_note_line_to_head (#186)
    - Eliminated a TODO comment regarding duplication in the
    `raw_add_note_line_to_head` function to improve code clarity and
    maintainability.
    
    topic:refactor-remove-outdated-todo

--- git_perf/src/git_interop.rs
+++ git_perf/src/git_interop.rs
@@ -245,7 +245,6 @@ fn raw_add_note_line_to_head(line: &str) -> Result<(), GitError> {
     )?;
 
     // Update current write branch with pending write
-    // TODO(kaihowl) duplication
     git_update_ref(unindent(
         format!(
             r#"
