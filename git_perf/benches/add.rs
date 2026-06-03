b754152457f6bbc969670329df7453b86f23caf1
Kai Howelmeyer <kai@hoewelmeyer.eu>
2026-06-03 12:03:30 +0200

    perf(ci): improve reliability of add_measurement/1 benchmark (#720)

--- git_perf/benches/add.rs
+++ git_perf/benches/add.rs
@@ -1,8 +1,9 @@
 use std::env::set_current_dir;
+use std::process::Command;
 
 use criterion::{criterion_group, criterion_main, BatchSize, BenchmarkId, Criterion, Throughput};
 use git_perf::git::git_interop::add_note_line_to_head;
-use git_perf::test_helpers::{empty_commit, hermetic_git_env, init_repo_simple};
+use git_perf::test_helpers::{empty_commit, hermetic_git_env};
 use tempfile::tempdir;
 
 fn prep_repo() -> tempfile::TempDir {
@@ -11,7 +12,13 @@ fn prep_repo() -> tempfile::TempDir {
     set_current_dir(temp_dir.path()).expect("Failed to change current path");
     hermetic_git_env();
 
-    init_repo_simple();
+    // Explicit --initial-branch for consistent behaviour across git versions.
+    assert!(Command::new("git")
+        .args(["init", "--initial-branch", "master"])
+        .output()
+        .expect("Failed to init git repo")
+        .status
+        .success());
 
     empty_commit();
 
@@ -20,7 +27,7 @@ fn prep_repo() -> tempfile::TempDir {
 
 fn add_measurements(c: &mut Criterion) {
     let mut group = c.benchmark_group("add_measurements");
-    group.sample_size(10);
+    group.sample_size(50);
     for num_measurements in [1, 50, 100].into_iter() {
         group.throughput(Throughput::Elements(num_measurements as u64));
         group.bench_with_input(
@@ -28,7 +35,7 @@ fn add_measurements(c: &mut Criterion) {
             &num_measurements,
             |b, &i| {
                 b.iter_batched(
-                    || prep_repo(),
+                    prep_repo,
                     |_temp_dir| {
                         for _ in 0..i {
                             add_note_line_to_head("some line measurement test").expect("Oh no");
@@ -56,7 +63,7 @@ fn add_multiple_measurements(c: &mut Criterion) {
             &num_measurements,
             |b, _i| {
                 b.iter_batched(
-                    || prep_repo(),
+                    prep_repo,
                     |_temp_dir| {
                         add_note_line_to_head(&lines).expect("failed to add lines");
                     },
